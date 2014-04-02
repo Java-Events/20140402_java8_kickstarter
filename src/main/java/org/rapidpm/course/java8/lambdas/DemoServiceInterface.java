0cd203f305419cc8f78450466be3ce8fb780c1bd
Sven Ruppert <sven.ruppert@rapidpm.org>
2014-04-02 08:58:26 +0200

    lambdas

---
+++ src/main/java/org/rapidpm/course/java8/lambdas/DemoServiceInterface.java
@@ -0,0 +1,18 @@
+package org.rapidpm.course.java8.lambdas;
+
+/**
+ *
+ * Legacy Interface - veraendern nicht moeglich
+ *
+ * Created by Sven Ruppert on 01.04.2014.
+ */
+public interface DemoServiceInterface {
+
+    public void addDemoAction(DemoAction action);
+
+    public void addDemoActionEndOfList(DemoAction action);
+
+    public void execute(Message msg);
+    public void executeActionTwo(Message msg);
+
+}
---
+++ src/main/java/org/rapidpm/course/java8/lambdas/legacy/Main.java
@@ -0,0 +1,57 @@
+package org.rapidpm.course.java8.lambdas.legacy;
+
+import org.rapidpm.course.java8.lambdas.DemoAction;
+import org.rapidpm.course.java8.lambdas.DemoServiceInterface;
+import org.rapidpm.course.java8.lambdas.Message;
+
+/**
+ * Created by ts40 on 01.04.2014.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        DemoServiceInterface demoService = new DemoService();
+
+        demoService.addDemoAction(new DemoAction() {
+            @Override
+            public void executeAction(Message message) {
+                System.out.println("001 - message.getMsg() = " + message.getMsg());
+            }
+
+            @Override
+            public void executeActionTwo(Message message) {
+
+            }
+
+        });
+        demoService.addDemoAction(new DemoAction() {
+            @Override
+            public void executeAction(Message message) {
+                System.out.println("002 - message.getMsg() = " + message.getMsg());
+            }
+
+            @Override
+            public void executeActionTwo(Message message) {
+
+            }
+
+        });
+        demoService.addDemoAction(new DemoAction() {
+            @Override
+            public void executeAction(Message message) {
+                System.out.println("003 - message.getMsg() = " + message.getMsg());
+            }
+
+            @Override
+            public void executeActionTwo(Message message) {
+
+            }
+
+        });
+
+        demoService.execute(new Message("Hello Legacy Service"));
+        demoService.executeActionTwo(new Message("Hello Legacy Service"));
+
+    }
+}
