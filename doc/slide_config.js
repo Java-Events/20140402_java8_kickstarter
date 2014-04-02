b25474d664dba578b307ece82a08c525bbea8290
Sven Ruppert <sven.ruppert@rapidpm.org>
2014-04-02 08:57:06 +0200

    init

---
+++ doc/slide_config.js
@@ -0,0 +1,37 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Java 8 <br> ready for the future',
+    subtitle: 'Streams and Lambdas in real life',
+    eventTitle: 'bei der JUG XXX',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/logo_sru.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]//,
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Sven Ruppert',
+    company: '',
+    gplus: 'https://plus.google.com/u/0/+SvenRuppert',
+    twitter: '@SvenRuppert - www.rapidpm.org',
+    www: 'http://www.rapidpm.org',
+    github: 'https://github.com/svenruppert'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
