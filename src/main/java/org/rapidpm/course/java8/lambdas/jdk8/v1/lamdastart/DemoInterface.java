6b6f02a37ac8111b09e9abb215db7f64b93d0786
Sven Ruppert <sven.ruppert@rapidpm.org>
2014-04-02 10:47:08 +0200

    lamda intro

---
+++ src/main/java/org/rapidpm/course/java8/lambdas/jdk8/v1/lamdastart/DemoInterface.java
@@ -0,0 +1,9 @@
+package org.rapidpm.course.java8.lambdas.jdk8.v1.lamdastart;
+
+/**
+ * Created by Sven Ruppert on 02.04.2014.
+ */
+@FunctionalInterface
+public interface DemoInterface {
+    public void doSomething();
+}
