d6c6a6889371adc85120470948ae4b8d3ea3b379
Sven Ruppert <sven.ruppert@rapidpm.org>
2014-04-03 09:42:04 +0200

    Streams 1.1 Uebung

---
+++ src/main/java/org/rapidpm/course/java8/streams/chap_1_1/CHAP_1_1_Uebung.java
@@ -0,0 +1,15 @@
+package org.rapidpm.course.java8.streams.chap_1_1;
+
+import org.rapidpm.course.java8.streams.Pair;
+import org.rapidpm.course.java8.streams.PairListGenerator;
+
+import java.util.List;
+
+/**
+ * Created by Sven Ruppert on 03.04.2014.
+ */
+public class CHAP_1_1_Uebung {
+
+    final List<Pair> generateDemoValues = new PairListGenerator(){}.generateDemoValues();
+
+}
